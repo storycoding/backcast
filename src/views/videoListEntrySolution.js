91363acfd1aa90ee2580c76bf1748a959f7aab9a
storycoding <youknownuno@gmail.com>
2017-12-12 16:40:03 -0800

    added solution code in different separate files to compare results

---
+++ src/views/videoListEntrySolution.js
@@ -0,0 +1,19 @@
+var VideoListEntryView = Backbone.View.extend({
+
+
+  events: {
+    'click .video-list-entry-title': 'handleClick'
+  },
+
+  handleClick: function() {
+    this.model.select();
+  },
+  
+  render: function() {
+    this.$el.html(this.template(this.model.attributes));
+    return this;
+  },
+
+  template: templateURL('src/templates/videoListEntry.html')
+
+});
